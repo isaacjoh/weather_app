c730f299bfe71677e966c07a03bd694b1e5fa9f7
Isaac Oh <isaac.oh@emotive.io>
2022-05-26 18:47:31 -0700

    Add libs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
-function App() {
+function Incrementer() {
+  const [city, setCity] = useState(" ");
+  const [weatherData, setWeatherData] = useState({
+    description: "",
+    temp: 0,
+    temp_max: 0,
+    temp_min: 0,
+  });
+
+  useEffect(() => {
+    Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9f2719b9da8433e4284c2819c1e75cc2`
+    )
+      .then((response) => {
+        console.log(response.data);
+        setWeatherData({
+          description: response.data.weather[0].description,
+          temp: response.data.main.temp,
+          temp_max: response.data.main.temp_max,
+          temp_min: response.data.main.temp_min,
+        });
+      })
+      .catch((err) => {
+        console.log("error here");
+      });
+  }, [city]);
+  console.log(weatherData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <div>
+          <input
+            type="text"
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+          />
+          <button>Search</button>
+        </div>
+        <div>{weatherData.description}</div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Incrementer;
